bc30367ce25528e7939189bdebd719aa0f08f016
niolap <nicolas@agoston.io>
2024-04-20 15:39:16 +0200

    progress

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./ssl/graphiql.agoston.dev.local.key'),
+      cert: fs.readFileSync('./ssl/graphiql.agoston.dev.local.crt'),
+    },
+  }
 })
